5cb5fc447b56052e8ddd1fae26e1f8e4f7f93561
Molnar Laszlo <laszlo.molnar25@gmail.com>
2020-08-30 22:07:28 +0200

    Initial commit

---
+++ Auth_basics/Controllers/UserController.cs
@@ -0,0 +1,90 @@
+﻿using Auth_basics.Entities;
+using Auth_basics.Helpers;
+using Auth_basics.Services;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace Auth_basics.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private IUserService _userService;
+        private AppSettings _appSettings;
+
+        public IEnumerable<string> Scopes { get; private set; }
+
+        public UsersController(IUserService userService, IOptions<AppSettings> appSettings)
+        {
+            _userService = userService;
+            _appSettings = appSettings.Value;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody]User userParam)
+        {
+            var user = _userService.Authenticate(userParam.Username, userParam.Password);
+
+            if (user == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            return Ok(user);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users = _userService.GetAll();
+            return Ok(users);
+        }
+
+        [AllowAnonymous]
+        [HttpGet("checktoken")]
+        public IActionResult CheckTokenIsValid([FromHeader]string authorization)
+        {
+            var token = authorization.Replace("Bearer ", "");
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+
+            var validationParameters = new TokenValidationParameters()
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero
+
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            SecurityToken validatedToken = null;
+            try
+            {
+                tokenHandler.ValidateToken(token, validationParameters, out validatedToken);
+            }
+            catch (SecurityTokenException)
+            {
+                return StatusCode(401, new { TokenIsVlaid = "false" });
+            }
+            catch (Exception e)
+            {
+
+                return StatusCode(111, new { message = e.ToString() });
+            }
+
+            //... manual validations return false if anything untoward is discovered
+            return Ok(validatedToken);
+        }
+    }
+}
